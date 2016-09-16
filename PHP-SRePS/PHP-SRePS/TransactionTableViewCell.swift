5a165e710ac1cc835f62587a80802789ed9633ef
Xuanson On <100582906@student.swin.edu.au>
2016-09-16 12:31:12 +1000

    *Create transaction cell.

---
+++ PHP-SRePS/PHP-SRePS/TransactionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TransactionTableViewCell.swift
+//  PHP-SRePS
+//
+//  Created by School on 20/09/2016.
+//  Copyright © 2016 swindp2. All rights reserved.
+//
+
+import UIKit
+
+class TransactionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
