490990001ffb50d8573e394c5c91b935139de3a2
s9998373 <9998373@student.swin.edu.au>
2016-09-18 22:31:49 +1000

    *Added custom date range selector functionality.

--- PHP-SRePS/PHP-SRePS/CustomTimeframeSelectorReportViewController.swift
+++ PHP-SRePS/PHP-SRePS/CustomTimeframeSelectorReportViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class CustomTimeframeSelectorReportViewController: UIViewController {
 
-    @IBOutlet weak var startDate: UIDatePicker!
-    @IBOutlet weak var endDate: UIDatePicker!
+    @IBOutlet weak var startDatePicker: UIDatePicker!
+    @IBOutlet weak var endDatePicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let currentDate = NSDate()
+        let yesterday = currentDate.dateByAddingTimeInterval(-(60 * 60 * 24))
+        startDatePicker.date = yesterday
+        endDatePicker.date = currentDate
         
         let nextButton = UIBarButtonItem.init(title: "Next", style: UIBarButtonItemStyle.Plain, target: self, action: #selector(CustomTimeframeSelectorReportViewController.nextButtonPressed))
         self.navigationItem.rightBarButtonItem = nextButton
@@ -27,7 +32,13 @@ class CustomTimeframeSelectorReportViewController: UIViewController {
     }
     
     func nextButtonPressed(){
+        let transactionsViewController = self.storyboard?.instantiateViewControllerWithIdentifier("TransactionsTableViewController") as! TransactionsTableViewController
+        transactionsViewController.transactionViewMode = TransactionViewMode.Specified
+        var dataSource:NSArray
         
+        dataSource = SalesDataSource.sharedManager.transactionsBetweenDates(startDatePicker.date, end: endDatePicker.date)
+        transactionsViewController.transactionList = dataSource
+        self.navigationController?.pushViewController(transactionsViewController, animated: true)
     }
 
 }
